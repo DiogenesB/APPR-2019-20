a118c5e3935651cc5a26b3961b222aa7e77b3c03
Tin Markon <tin.markon@stroka.si>
2020-02-10 16:36:21 +0100

    Uvoz in obdelava podatkov

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,55 +1,79 @@
-# 2. faza: Uvoz podatkov
+library(readr)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+#Uvoz prve tabele
+stolpci_1 <- c("kljuc.narocila", "kljuc.uporabnika", "status.narocila", "cas.nakupa", 
+               "odobren.cas.nakupa", "cas.ko.je.posiljko.prejel.partner", "dejanski.cas.dostave", "predviden.cas.dostave" )
+tabela_narocil <- read_csv("podatki/olist_orders_dataset.csv", na = c("", " ", "NA")) %>% drop_na()
+colnames(tabela_narocil) <- stolpci_1
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    if (is.character(tabela[[col]])) {
-      tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-    }
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
-                    locale=locale(encoding="Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- iconv("Sveti Jurij ob Ščavnici", to="UTF-8")
-  data <- data %>% gather(`1`:`4`, key="velikost.druzine", value="stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- parse_factor(data$obcina, levels=obcine)
-  return(data)
-}
+#Uvoz druge tabele
+stolpci_2 <- c("kljuc.narocila", "zaporedje.placila", "tip.placila", "stevilo.obrokov.placila", "vrednost.placila")
+tabela_vrst_placil <- read_csv("podatki/olist_order_payments_dataset.csv")
+colnames(tabela_vrst_placil) <- stolpci_2
 
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
+#Uvoz tretje tabele
+stolpci_3 <- c("kljuc.izdelka", "kategorija.izdelka", "dolzina.naziva.izdelka", "dolzina.opisa.izdelka", 
+               "stevilo.objavljenih.fotografij.izdelka", "masa.izdelka (g)", "dolzina", "visina", "sirina")
+tabela_produktov <- read_csv("podatki/olist_products_dataset.csv", na = c("", " ", "NA")) %>% drop_na() 
+colnames(tabela_produktov) <- stolpci_3
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+#Uvoz četrte tabele
+stolpci_4 <- c("original", "prevod")
+tabela_prevodov <- read_csv("podatki/product_category_name_translation.csv")
+colnames(tabela_prevodov) <- stolpci_4
+
+
+#Združevanje 1. in 2. tabele
+narocila <- left_join(tabela_narocil, tabela_vrst_placil, by = c("kljuc.narocila"), copy=FALSE)
+narocila <- narocila %>% drop_na()
+narocila$status.narocila <- as.factor(narocila$status.narocila)
+narocila$tip.placila <- as.factor(narocila$tip.placila)
+narocila$zaporedje.placila <- NULL
+narocila$kljuc.uporabnika <- NULL
+narocila$odobren.cas.nakupa <- NULL
+narocila$cas.ko.je.posiljko.prejel.partner <- NULL
+narocila$pravocasnost = narocila$predviden.cas.dostave >= narocila$dejanski.cas.dostave
+
+
+#Tabela ki opisuje promet na platformi v letu 2017
+promet_2017 <- narocila %>%
+  select(dejanski.cas.dostave, vrednost.placila) %>%
+  filter(dejanski.cas.dostave >= "2016-12-31" & dejanski.cas.dostave <= "2018-01-01") %>%
+  mutate(mesec.dostave = month(dejanski.cas.dostave)) %>%
+  select(mesec.dostave, vrednost.placila) %>%
+  group_by(mesec.dostave) %>%
+  summarise(vrednost.placila = sum(vrednost.placila))
+
+
+#Tabela, ki opisuje promet na platformi v decembru leta 2017
+december <- narocila %>%
+  select(cas.nakupa, vrednost.placila) %>%
+  filter(cas.nakupa >= "2017-12-01" & cas.nakupa <= "2017-12-31") %>%
+  mutate(dan.narocila = day(cas.nakupa)) %>%
+  select(dan.narocila, vrednost.placila) %>%
+  group_by(dan.narocila) %>%
+  summarise(vrednost.placila = sum(vrednost.placila))
+
+#Tabela, ki opisuje promet na platformi v novembru leta 2017
+november <- narocila %>%
+  select(cas.nakupa, vrednost.placila) %>%
+  filter(cas.nakupa >= "2017-11-01" & cas.nakupa <= "2017-11-30") %>%
+  mutate(dan.narocila = day(cas.nakupa)) %>%
+  select(dan.narocila, vrednost.placila) %>%
+  group_by(dan.narocila) %>%
+  summarise(vrednost.placila = sum(vrednost.placila))
+
+
+#Združevanje 3. in 4. tabele
+izdelki <- left_join(tabela_produktov, tabela_prevodov, by = c("kategorija.izdelka"="original"), copy=FALSE)
+izdelki$kategorija.izdelka <- NULL
+izdelki$dolžina.naziva.izdelka <- NULL
+izdelki$dolžina.opisa.izdelka<- NULL
+izdelki$število.objavljenih.fotografij.izdelka <- NULL
+names(izdelki)[6] <- c("kategorija.izdelka")
