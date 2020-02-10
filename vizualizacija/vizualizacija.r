ea24fe640a70933896699c2e35e73860ea51c928
Tin Markon <tin.markon@stroka.si>
2020-02-10 17:59:28 +0100

    Dodan delujoč zemljevid

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -12,34 +12,43 @@ library(rgeos)
 source("lib/uvozi.zemljevid.r")
 
 meseci <- c("Januar", "Februar", "Marec", "April", "Maj", "Junij", "Julij", "Avgust", "September", "Oktober", "November", "December")
-graf_promet <- ggplot(promet_2017, aes(x = promet_2017$mesec.dostave, y = promet_2017$vrednost.placila / 1000)) + geom_col() + 
-  labs(title = "Pregled prometa v letu 2017", x = "Mesec", y = "Skupni promet (v 1000€)") + scale_x_continuous(breaks = 1:12, labels = meseci )
+graf_promet <- ggplot(promet_2017, aes(x = promet_2017$mesec.dostave, y = promet_2017$vrednost.placila / 1000)) + 
+  geom_col() + 
+  labs(title = "Pregled prometa v letu 2017", x = "Mesec", y = "Skupni promet (v 1000€)") +
+  scale_x_continuous(breaks = 1:12, labels = meseci )
 
-graf_tipi <- ggplot(narocila, aes(narocila$tip.placila)) + geom_bar() +
-  labs(title = "Plačilna sredstva", x = "Tip plačilnega sredstva", y = "Število nakupov") + scale_x_discrete() + coord_flip()
+graf_tipi <- ggplot(narocila, aes(narocila$tip.placila)) + 
+  geom_bar() +
+  labs(title = "Plačilna sredstva", x = "Tip plačilnega sredstva", y = "Število nakupov") + 
+  scale_x_discrete() + 
+  coord_flip()
 
 graf_gostot <- ggplot(narocila, aes(x = narocila$vrednost.placila, color=narocila$tip.placila)) +
   geom_density()  +
   scale_x_log10() +
   labs(title = "Gostota cen glede na plačilno sredstvo", x = "Gostota", y = "Vrednost plačila")
 
-graf_december <- ggplot(december, aes(x=december$dan.narocila, y = december$vrednost.placila)) + geom_col() +
-  labs(title = "Pregled prometa v decembru 2017", x = "Dan", y = "Skupni promet") + scale_x_continuous(breaks = 1:31)
+graf_december <- ggplot(december, aes(x=december$dan.narocila, y = december$vrednost.placila)) +
+  geom_col() +
+  labs(title = "Pregled prometa v decembru 2017", x = "Dan", y = "Skupni promet") +
+  scale_x_continuous(breaks = 1:31)
 
-graf_november <- ggplot(november, aes(x=november$dan.narocila, y=november$vrednost.placila)) + geom_col() +
-  labs(title = "Pregled prometa v novembru 2017", x = "Dan", y = "Skupni promet") + scale_x_continuous(breaks = 1:30)
+graf_november <- ggplot(november, aes(x=november$dan.narocila, y=november$vrednost.placila)) +
+  geom_col() +
+  labs(title = "Pregled prometa v novembru 2017", x = "Dan", y = "Skupni promet") + 
+  scale_x_continuous(breaks = 1:30)
 
 
 # TODO: popravi pozicioniranje grafov
 
 
-#brazilija <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_BRA_shp.zip", "gadm36_BRA_0", force=TRUE) %>% fortify()
+brazilija <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_BRA_shp.zip", "gadm36_BRA_0", force=FALSE) %>% fortify()
 
 
-#map <- ggplot(brazilija, aes(x=long, y=lat)) +
-#  geom_polygon(aes(group=group)) +
-#  geom_point(aes(x=lokacija_prodajalcev$zemljepisna.dolzina, y=lokacija_prodajalcev$zemljepisna.sirina), lokacija_prodajalcev, col="red") +
+map <- ggplot(brazilija, aes(x=long, y=lat)) +
+ geom_polygon(aes(group=group)) +
+# geom_point(aes(x=lokacija_prodajalcev$zemljepisna.dolzina, y=lokacija_prodajalcev$zemljepisna.sirina), lokacija_prodajalcev, col="red") +
 #  geom_point(aes(x=lokacija_kupcev$zemljepisna.dolzina, y=lokacija_kupcev$zemljepisna.sirina), lokacija_kupcev, col="blue")
-#  labs(title ="Vizualizacija prodajalcev in kupcev") 
+  labs(title ="Vizualizacija prodajalcev in kupcev") 
 
 
