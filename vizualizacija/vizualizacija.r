b73e91be026f074ca73ded5a393e9834bb5da10b
Tin Markon <tin.markon@stroka.si>
2020-02-10 16:41:48 +0100

    Uvoz vizualizacij

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,12 +1,42 @@
-# 3. faza: Vizualizacija podatkov
+library(ggplot2)
+library(ggvis)
+library(dplyr)
+library(rgdal)
+library(mosaic)
+library(maptools)
+library(ggmap)
+library(mapproj)
+library(munsell)
+library(maps)
+library(rgeos)
+source("lib/uvozi.zemljevid.r")
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
+meseci <- c("Januar", "Februar", "Marec", "April", "Maj", "Junij", "Julij", "Avgust", "September", "Oktober", "November", "December")
+graf_promet <- ggplot(promet_2017, aes(x = promet_2017$mesec.dostave, y = promet_2017$vrednost.placila / 1000)) + geom_col() + 
+  labs(title = "Pregled prometa v letu 2017", x = "Mesec", y = "Skupni promet (v 1000€)") + scale_x_continuous(breaks = 1:12, labels = meseci )
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+graf_tipi <- ggplot(narocila, aes(narocila$tip.placila)) + geom_bar() +
+  labs(title = "Plačilna sredstva", x = "Tip plačilnega sredstva", y = "Število nakupov") + scale_x_discrete() + coord_flip()
+
+graf_gostot <- ggplot(narocila, aes(x = narocila$vrednost.placila, color=narocila$tip.placila)) + geom_density()  + scale_x_log10()
+
+graf_december <- ggplot(december, aes(x=december$dan.narocila, y = december$vrednost.placila)) + geom_col() +
+  labs(title = "Pregled prometa v decembru 2017", x = "Dan", y = "Skupni promet") + scale_x_continuous(breaks = 1:31)
+
+graf_november <- ggplot(november, aes(x=november$dan.narocila, y=november$vrednost.placila)) + geom_col() +
+  labs(title = "Pregled prometa v novembru 2017", x = "Dan", y = "Skupni promet") + scale_x_continuous(breaks = 1:30)
+
+
+# TODO: popravi pozicioniranje grafov
+
+
+brazilija <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_BRA_shp.zip", "gadm36_BRA_0", force=TRUE) %>% fortify()
+
+
+map <- ggplot(brazilija, aes(x=long, y=lat)) +
+  geom_polygon(aes(group=group)) +
+  geom_point(aes(x=lokacija_prodajalcev$zemljepisna.dolzina, y=lokacija_prodajalcev$zemljepisna.sirina), lokacija_prodajalcev, col="red") +
+  geom_point(aes(x=lokacija_kupcev$zemljepisna.dolzina, y=lokacija_kupcev$zemljepisna.sirina), lokacija_kupcev, col="blue")
+  labs(title ="Vizualizacija prodajalcev in kupcev") 
+
+
