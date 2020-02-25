0b69703cfd335d7d231489ceb5c74965ced20a82
Tin Markon <tin.markon@stroka.si>
2020-02-25 16:32:49 +0100

    Testno dodani grafi pred analizo in prva linearna regresija

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,3 +1,15 @@
-##TODO regresija
-##TODO cluster analiza kupcev na zemljevidu
-##TODO regresija glede na dolžino in širino (zemljepisno) in kako dolgo je trajalo da je paket prišel
+### Grafična analiza
+
+#Ali se ena količina spremninja linearno na drugo koločino
+scatter.smooth(x=lokacija.narocil$razdalja, y=lokacija.narocil$trajanje)
+
+
+#Iščemo outlier-je
+par(mfrow=c(1, 2))  # divide graph area in 2 columns
+boxplot(lokacija_narocil$razdalja, main="Razdalja", sub=paste("Outlier rows: ", boxplot.stats(lokacija_narocil$razdalja)$out))  # box plot for 'speed'
+boxplot(cars$dist, main="Distance", sub=paste("Outlier rows: ", boxplot.stats(lokacija_narocil$trajanje)$out))  # box plot for 'distance'
+
+
+linearna_regresija <- lm(trajanje ~ razdalja, data=lokacija_narocil)
+print(linearna_regresija)
+summary(linearna_regresija)
