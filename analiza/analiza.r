e5410741701af791954cc29c98d3ff2caec7477f
Tin Markon <tin.markon@stroka.si>
2020-02-26 16:06:44 +0100

    Dodana analiza in primer linearne regresije

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,13 +1,22 @@
 ### Grafična analiza
 
-#Ali se ena količina spremninja linearno na drugo koločino
-scatter.smooth(x=lokacija.narocil$razdalja, y=lokacija.narocil$trajanje)
+#Pogledamo v kakšnem odnosu sta količini
+odnos_kolicin <- ggplot(lokacija_narocil, aes(x=lokacija_narocil$razdalja, y=lokacija_narocil$trajanje)) +
+  geom_point()
 
 
-#Iščemo outlier-je
-par(mfrow=c(1, 2))  # divide graph area in 2 columns
-boxplot(lokacija_narocil$razdalja, main="Razdalja", sub=paste("Outlier rows: ", boxplot.stats(lokacija_narocil$razdalja)$out))  # box plot for 'speed'
-boxplot(cars$dist, main="Distance", sub=paste("Outlier rows: ", boxplot.stats(lokacija_narocil$trajanje)$out))  # box plot for 'distance'
+
+#Pogledamo kako sta količini korelirani
+cor_kolicin <- cor(lokacija_narocil$trajanje, lokacija_narocil$razdalja)
+
+#Pogledamo gostoto obeh spremenljivk
+gostota_trajanja <- ggplot(lokacija_narocil, aes(x=lokacija_narocil$trajanje)) +
+  geom_density() +
+  scale_x_log10() 
+gostota_razdalja <- ggplot(lokacija_narocil, aes(x=lokacija_narocil$razdalja)) +
+  geom_density() + 
+  scale_x_log10()
+
 
 
 linearna_regresija <- lm(trajanje ~ razdalja, data=lokacija_narocil)
