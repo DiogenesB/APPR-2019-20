b9920f4a3b54a11b51ed34fc24583f8e2be7f6a3
Tin Markon <tin.markon@stroka.si>
2020-03-02 17:35:29 +0100

    Ustvarjanje zemljevida na podlagi inputa

--- shiny/server.R
+++ shiny/server.R
@@ -1,10 +1,11 @@
 library(shiny)
+library(dplyr)
 
 shinyServer(function(input, output) {
   output$zemljevid <- renderPlot(
     ggplot(brazilija, aes(x=long, y=lat)) +
     geom_polygon(aes(group=group)) +
-    geom_point(aes(x=zemljepisna.dolzina, y=zemljepisna.sirina, color=tip.placila), data = distinct(select(lokacija_kupcev, kljuc.uporabnika, zemljepisna.sirina, zemljepisna.dolzina, tip.placila), kljuc.uporabnika, .keep_all = TRUE)) + 
+    geom_point(aes(x=zemljepisna.dolzina, y=zemljepisna.sirina, color=tip.placila), data = distinct(select(lokacija_kupcev, kljuc.uporabnika, zemljepisna.sirina, zemljepisna.dolzina, tip.placila), kljuc.uporabnika, .keep_all = TRUE) %>% filter(tip.placila==input$tip)) + 
     geom_point(aes(x=zemljepisna.dolzina, y=zemljepisna.sirina), data = lokacija_prodajalcev, col="red", alpha = 0.05) +
     coord_cartesian(xlim = c(-73.98283055, -34.79314722), ylim = c(-33.75116944, 5.27438888)) +
     labs(title ="Vizualizacija prodajalcev in kupcev", x = "Zemljepisna dolžina", y = "Zemljepisna širina") 
