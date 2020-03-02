fc88e0f463bfe73dcd1c731966cb1776548e0deb
Tin Markon <tin.markon@stroka.si>
2020-03-02 17:34:50 +0100

    Testno dodani tipi plačil

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,6 +1,6 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  checkboxGroupInput(inputId="tip", label="Izberite tip plačila"),
+  checkboxGroupInput(inputId="tip", label="Izberite tip plačila", choiceNames=c("Boleto", "Kreditna kartica", "Debetna kartica", "Bon"), choiceValues=c("boleto", "credit_card", "debit_card", "voucher")),
   plotOutput("zemljevid") ## TODO popravi razmerja zemljevida
 ))
