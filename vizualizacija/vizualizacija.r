5440ca15605397da1a968de6c28a8e0453a43284
Tin Markon <tin.markon@stroka.si>
2020-07-15 10:56:18 +0200

    Izbrisane lokacije kupcev - preveč podatkov

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -43,8 +43,7 @@ brazilija <- uvozi.zemljevid("https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36
 
 map <- ggplot(brazilija, aes(x=long, y=lat)) +
   geom_polygon(aes(group=group)) +
-  geom_point(aes(x=zemljepisna.dolzina, y=zemljepisna.sirina, color=tip.placila), data = distinct(select(lokacija_kupcev, kljuc.uporabnika, zemljepisna.sirina, zemljepisna.dolzina, tip.placila), kljuc.uporabnika, .keep_all = TRUE)) + 
   geom_point(aes(x=zemljepisna.dolzina, y=zemljepisna.sirina), data = lokacija_prodajalcev, col="red", alpha = 0.05) +
   coord_cartesian(xlim = c(-73.98283055, -34.79314722), ylim = c(-33.75116944, 5.27438888)) +
-  labs(title ="Vizualizacija prodajalcev in kupcev", x = "Zemljepisna dolžina", y = "Zemljepisna širina") 
-##TODO poglej GeoSpatial analizo te baze na Kaggle-u
+  labs(title ="Vizualizacija prodajalcev", x = "Zemljepisna dolžina", y = "Zemljepisna širina") 
+
