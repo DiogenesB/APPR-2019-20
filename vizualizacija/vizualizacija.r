7cc444185fc9637b4b74fcfab69fc6cd0001fc75
Tin Markon <tin.markon@stroka.si>
2020-09-11 11:23:23 +0200

    Rotirane oznake na prvem grafu

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -15,7 +15,8 @@ meseci <- c("Januar", "Februar", "Marec", "April", "Maj", "Junij", "Julij", "Avg
 graf_promet <- ggplot(promet_2017, aes(x = promet_2017$mesec.dostave, y = promet_2017$vrednost.placila / 1000)) + 
   geom_col() + 
   labs(title = "Pregled prometa v letu 2017", x = "Mesec", y = "Skupni promet (v 1000€)") +
-  scale_x_continuous(breaks = 1:12, labels = meseci )
+  scale_x_continuous(breaks = 1:12, labels = meseci ) +
+  theme(axis.text.x = element_text(angle = 90))
 
 graf_tipi <- ggplot(narocila, aes(narocila$tip.placila)) + 
   geom_bar() +
